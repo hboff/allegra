60477807d0bf87983d7edb07f9cf15681acbea16
hboff <oskar@baucampus.de>
2023-02-01 18:08:43 +0100

    12

--- app/Http/Controllers/YourController.php
+++ app/Http/Controllers/YourController.php
@@ -7,32 +7,36 @@ use Illuminate\Support\Facades\Route;
 
 
 
+
 class YourController extends Controller
 {
 public function registerRoutes()
-  {
+{
     $outerValues = [
       'immobilienbewertung-duisburg.com',
       'immobilienbewertung-bochum.com',
     ];
 
-$innerValues = [
-'datenschutzerklaerung',
-'welcome',
-'index',
-];
+    $innerValues = [
+      'datenschutzerklaerung',
+      'welcome',
+      'index',
+    ];
 
-foreach ($outerValues as $outerValue) {
-    Route::group(['domain' => 'immobilienbewertung-duisburg.com'], function () use ($outerValue, $innerValues) { 
+    foreach ($outerValues as $outerValue) {
+        Route::group(['domain' => $outerValue], function () use ($outerValue, $innerValues) { 
 
-  foreach ($innerValues as $innerValue) {
-    Route::get("/$innerValue", function () use ($outerValue, $innerValue) {
-      return view($innerValue, compact('outerValue', 'innerValue'));
-    });       
+        foreach ($innerValues as $innerValue) {
+            Route::get("/$innerValue", function () use ($outerValue, $innerValue) {
+                return view($innerValue, compact('outerValue', 'innerValue'));
+            });       
+        }
+    }); 
   }
-}); 
 }
+
 }
-}
+
+
 
 
