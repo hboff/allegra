424452fd176e4b5cd72db26ca5624c9a6ce17d86
hboff <oskar@baucampus.de>
2023-02-01 19:31:20 +0100

    12

--- routes/web.php
+++ routes/web.php
@@ -1,65 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\OskarController;
-(new App\Http\Controllers\YourController)->registerRoutes(); 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-//Route::group(['domain' => 'immobilienbewertung-duisburg.com'], function () {
-//Route::get('/', [OskarController::class, 'index']);
-//Route::get('/impressum', [OskarController::class, 'impressum']);
-//Route::get('/datenschutzerklaerung', [OskarController::class, 'datenschutzerklaerung']);
-//Route::get('/gewerbeimmobilien', [OrteController::class, 'gewerbeimmobilien']);
-//
-//$pages = array('welcome', 'contact', 'faqs');  
-//foreach ($pages as $page) {
-//    Route::get($page, function () use ($page) { 
-//        return view($page);
-//    }); 
-//}
-//});
 
+// Define the domains
+$domains = [
+    'immobilienbewertung-bochum.com',
+    'immobilienbewertung-duisburg.com',
+];
 
+// Define the views
+$views = [
+    'datenschutzerklaerung',
+    'welcome',
+    'index',
+];
 
-
-//Route::group(['domain' => 'immobilienbewertung-bochum.com'], function () {
-//Route::get('/', [OskarController::class, 'index']);
-//Route::get('/impressum', [OskarController::class, 'impressum']);
-//Route::get('/datenschutzerklaerung', [OskarController::class, 'datenschutzerklaerung']);
-//});
-//Route::get('/', [OrteController::class, 'index'], function () {
-//});
-//Route::get('/schimmelpilz', [OrteController::class, 'schimmelpilz'], function () {
-//});
-//Route::get('/gewerbeimmobilien', [OrteController::class, 'gewerbeimmobilien'], function () {
-//});
-//Route::get('/grundstuecke-und-rechte', [OrteController::class, 'grundstuecke'], function () {
-//});
-//Route::get('/landwirtschaftliche-immobilien', [OrteController::class, 'landwirtschaftliche'], function () {
-//});
-//Route::get('/sonderimmobilien', [OrteController::class, 'sonderimmobilien'], function () {
-//});
-//Route::get('/wohnimmobilien', [OrteController::class, 'wohnimmobilien'], function () {
-//});
-//Route::get('/verkehrswertverfahren', [OrteController::class, 'verkehrswertverfahren'], function () {
-//});
-//Route::get('/sachwertverfahren', [OrteController::class, 'sachwertverfahren'], function () {
-//});
-//Route::get('/ertragswertverfahren', [OrteController::class, 'ertragswertverfahren'], function () {
-//});
-//Route::get('/ueber-uns', [OrteController::class, 'ueberuns'], function () {
-//});
-
-//Route::get('/impressum', [OrteController::class, 'impressum'], function () {
-//});
-//Route::get('/datenschutzerklaerung', [OrteController::class, 'datenschutzerklaerung'], function () {
-//});
+// Loop for handling routes
+foreach ($domains as $domain) {
+    Route::group(['domain' => $domain], function() use ($views) {
+        foreach ($views as $view) {
+            Route::view("/$view", "$view");
+        }
+    });
+}
 
