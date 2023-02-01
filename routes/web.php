4c617f91fe54cc3aee0d61a386f31392e0334c62
hboff <oskar@baucampus.de>
2023-02-01 13:41:57 +0100

    1

--- routes/web.php
+++ routes/web.php
@@ -12,35 +12,31 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return 'Hello World';
+
+
+Route::get('/', [OrteController::class, 'index'], function () {
 });
-
-//Route::get('/', [OrteController::class, 'index'], function () {
-//});
-//Route::get('/schimmelpilz', [OrteController::class, 'schimmelpilz'], function () {
-//});
-//Route::get('/gewerbeimmobilien', [OrteController::class, 'gewerbeimmobilien'], function () {
-//});
-//Route::get('/grundstuecke-und-rechte', [OrteController::class, 'grundstuecke'], function () {
-//});
-//Route::get('/landwirtschaftliche-immobilien', [OrteController::class, 'landwirtschaftliche'], function () {
-//});
-//Route::get('/sonderimmobilien', [OrteController::class, 'sonderimmobilien'], function () {
-//});
-//Route::get('/wohnimmobilien', [OrteController::class, 'wohnimmobilien'], function () {
-//});
-//Route::get('/verkehrswertverfahren', [OrteController::class, 'verkehrswertverfahren'], function () {
-//});
-//Route::get('/sachwertverfahren', [OrteController::class, 'sachwertverfahren'], function () {
-//});
-//Route::get('/ertragswertverfahren', [OrteController::class, 'ertragswertverfahren'], function () {
-//});
-//Route::get('/ueber-uns', [OrteController::class, 'ueberuns'], function () {
-//});
-
-//Route::get('/impressum', [OrteController::class, 'impressum'], function () {
-//});
-//Route::get('/datenschutzerklaerung', [OrteController::class, 'datenschutzerklaerung'], function () {
-//});
-
+Route::get('/schimmelpilz', [OrteController::class, 'schimmelpilz'], function () {
+});
+Route::get('/gewerbeimmobilien', [OrteController::class, 'gewerbeimmobilien'], function () {
+});
+Route::get('/grundstuecke-und-rechte', [OrteController::class, 'grundstuecke'], function () {
+});
+Route::get('/landwirtschaftliche-immobilien', [OrteController::class, 'landwirtschaftliche'], function () {
+});
+Route::get('/sonderimmobilien', [OrteController::class, 'sonderimmobilien'], function () {
+});
+Route::get('/wohnimmobilien', [OrteController::class, 'wohnimmobilien'], function () {
+});
+Route::get('/verkehrswertverfahren', [OrteController::class, 'verkehrswertverfahren'], function () {
+});
+Route::get('/sachwertverfahren', [OrteController::class, 'sachwertverfahren'], function () {
+});
+Route::get('/ertragswertverfahren', [OrteController::class, 'ertragswertverfahren'], function () {
+});
+Route::get('/ueber-uns', [OrteController::class, 'ueberuns'], function () {
+});
+Route::get('/impressum', [OrteController::class, 'impressum'], function () {
+});
+Route::get('/datenschutzerklaerung', [OrteController::class, 'datenschutzerklaerung'], function () {
+});
